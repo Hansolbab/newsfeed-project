17123446330a5f679a30174acb5c81a8f7bbe3f3
hansolChoi29 <ggksthf29@gmail.com>
2025-08-10 00:12:07 +0900

    :sparkles: feat: dto에 기존비밀번호, 새비밀번호, 비밀번호 확인 넣음

--- src/main/java/com/example/newsfeedproject/myinfo/dto/PasswordResetRequestDto.java
+++ src/main/java/com/example/newsfeedproject/myinfo/dto/PasswordResetRequestDto.java
@@ -1,4 +1,23 @@
 package com.example.newsfeedproject.myinfo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PasswordResetRequestDto {
+    @NotBlank(message = "기존 비밀번호를 입력해 주세요")
+    private String oldPassword;
+
+    @NotBlank(message = "새 비밀번호를 입력해 주세요")
+    private String newPassword;
+
+    @NotBlank(message = "비밀번호 확인란을 입력해 주세요")
+    private String newConfirmPassword;
+
+    public boolean isNewPasswordMatch(){
+        return newPassword.equals(newConfirmPassword);
+    }
+
 }
