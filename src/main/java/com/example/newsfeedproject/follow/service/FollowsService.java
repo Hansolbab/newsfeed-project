9e8baae493d1a0ba827cc6b42d5c0f70714c6f0b
Beforejamni <beforejamn1@gmail.com>
2025-08-09 11:01:10 +0900

    ✨ feat :공통 검증 메서드로 관리

--- src/main/java/com/example/newsfeedproject/follow/service/FollowsService.java
+++ src/main/java/com/example/newsfeedproject/follow/service/FollowsService.java
@@ -18,18 +18,17 @@ public class FollowsService {
     private final UsersRepository usersRepository;
 
 
+
+    //팔로우 서비스
     public void follow(Long meId, Long userId) {
 
-        if(meId.equals(userId)) { // 같으면 나랑 나를 팔로우
-            throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
-        }
+        validId(meId, userId);
 
         //팔로우 할 사람이 있는지
-        Users me = usersRepository.findById(meId).orElseThrow(() -> new IllegalArgumentException("현재 유저가 없습니다."));
-
+        Users me = usersRepository.getReferenceById(meId);
 
         //팔로우 당할 사람이 있는지
-        Users followee = usersRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("대상 유저가 없습니다."));
+        Users followee = usersRepository.getReferenceById(userId);
 
 
         Follows relation = followsRepository
@@ -51,23 +50,42 @@ public class FollowsService {
         followsRepository.save(relation);
 
     }
-
+    //언팔로우 서비스
     public void unfollow(Long meId, Long userId) {
 
-        if(meId.equals(userId)) {
+        validId(meId, userId);
+
+        Users me = usersRepository.getReferenceById(meId); //  Id 값만 저장한 프록시 객체
+
+        Users followee = usersRepository.getReferenceById(userId);
+
+        Follows relation = followsRepository
+                    .findByFollowerAndFollowee(me, followee) // 팔로우 이력 확인 -> Users로 보냈지만, id값으로 판별해서 프록시라도 괜찮
+                    .orElseThrow( () -> new IllegalArgumentException("팔로우를 한 적이 없습니다.")); // 없으면 이미 언팔로우
+
+        if(!relation.isIsFollowed()) return; // 이력이 있는데 언팔로우 상태이면 그대로 둠
+
+        relation.setFollowed(false); // 이력이 있는데 팔로우 상태면 언팔로우 변경
+    }
+
+
+    //공통 검증
+    private void validId(Long meId, Long userId) {
+        if (meId == null || userId == null) {
+            throw new IllegalArgumentException("해당 유저들이 null 입니다.");
+        }
+
+        if (meId.equals(userId)) {
             throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
         }
 
-        Users me = usersRepository.findById(meId).orElseThrow(() -> new IllegalArgumentException("현재 유저가 없습니다."));
+        if (!usersRepository.existsById(meId)) {
+            throw new IllegalArgumentException("현재 유저가 없습니다.");
+        }
 
-        Users followee = usersRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("대상 유저가 없습니다."));
-
-        Follows relation = followsRepository
-                    .findByFollowerAndFollowee(me, followee)
-                    .orElseThrow( () -> new IllegalArgumentException("팔로우를 한 적이 없습니다."));
-
-        if(!relation.isIsFollowed()) return;
-
-        relation.setFollowed(false);
+        if (!usersRepository.existsById(userId)) {
+            throw new IllegalArgumentException("대상 유저가 없습니다.");
+        }
     }
+
 }
