299328dcbe8ab7248121f5c60341eaef0dd1e4c7
hansolChoi29 <ggksthf29@gmail.com>
2025-08-10 14:53:19 +0900

    :sparkles: feat: 비밀번호 컨트롤러와 서비스 추가됨(인증상태 403)

--- src/main/java/com/example/newsfeedproject/myinfo/controller/PasswordResetController.java
+++ src/main/java/com/example/newsfeedproject/myinfo/controller/PasswordResetController.java
@@ -1,4 +1,34 @@
 package com.example.newsfeedproject.myinfo.controller;
 
+import com.example.newsfeedproject.myinfo.dto.PasswordResetRequestDto;
+import com.example.newsfeedproject.myinfo.service.PasswordResetService;
+import com.example.newsfeedproject.users.entity.Users;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/myinfo")
+@RequiredArgsConstructor
 public class PasswordResetController {
+    private final PasswordResetService passwordResetService;
+
+    @PostMapping("password")
+    public ResponseEntity<String> resetPassword(Authentication authentication, @Valid @RequestBody PasswordResetRequestDto dto) {
+
+        //비밀번호가 일치하는지 확인
+        if(!dto.isNewPasswordMatch()){
+            return ResponseEntity.badRequest().body("새 비밀번호와 비밀번호 확인이 일치하지 않습니다. ");
+        }
+
+        //비밀번호 재설정 처리
+        passwordResetService.resetPassword(authentication, dto.getOldPassword(),  dto.getNewPassword());
+
+        return ResponseEntity.ok("비밀전호가 성공적으로 변경되었습니다.");
+    }
 }
