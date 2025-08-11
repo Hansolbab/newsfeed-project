93c4af3719093ee746dd01b7feab20e7c59096bb
JIHUN KWAK <rhkrwlgns12@naver.com>
2025-08-11 12:18:30 +0900

    :fire: delete: feedimg 패키지 삭제

--- src/main/java/com/example/newsfeedproject/feedimg/repository/FeedImgRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.newsfeedproject.feedimg.repository;
-
-import com.example.newsfeedproject.feedimg.entity.FeedImg;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedImgRepository extends JpaRepository<FeedImg, Long> {
-}
