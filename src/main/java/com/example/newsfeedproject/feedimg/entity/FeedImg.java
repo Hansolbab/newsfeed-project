83a00ddcf89db158b93cf4a75c3bb19d5183f474
Beforejamni <beforejamn1@gmail.com>
2025-08-11 14:11:52 +0900

    FeedImg.jave

--- src/main/java/com/example/newsfeedproject/feedimg/entity/FeedImg.java
+++ src/main/java/com/example/newsfeedproject/feedimg/entity/FeedImg.java
@@ -1,9 +1,15 @@
 package com.example.newsfeedproject.feedimg.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class FeedImg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedId;
 
 }
