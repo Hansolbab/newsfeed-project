a1faf42c4bb53046c46ad423132501e868fdcc1a
hansolChoi29 <ggksthf29@gmail.com>
2025-08-11 11:44:32 +0900

    :bug: 에러사항 변경 FeedImg 2차

--- src/main/java/com/example/newsfeedproject/feedImg/entity/FeedImg.java
+++ src/main/java/com/example/newsfeedproject/feedImg/entity/FeedImg.java
@@ -1,9 +1,9 @@
-package com.example.newsfeedproject.policy.entity;
+package com.example.newsfeedproject.feedImg.entity;
 
 import jakarta.persistence.*;
 
 @Entity
-public class Policy {
+public class FeedImg {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/newsfeedproject/policy/repository/PolicyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.newsfeedproject.policy.repository;
-
-import com.example.newsfeedproject.policy.entity.Policy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
