8cd65fecac09094129058593fbf967aeb8bd1ffc
hansolChoi29 <ggksthf29@gmail.com>
2025-08-13 00:12:18 +0900

    :memo: chore: 상수에 대한 설명 추가

--- src/main/java/com/example/newsfeedproject/myinfo/service/ProfileImageService.java
+++ src/main/java/com/example/newsfeedproject/myinfo/service/ProfileImageService.java
@@ -14,7 +14,7 @@ public class ProfileImageService {
     private final UsersRepository usersRepository;
 
     //TODO: 이미 UserEntity에 기본값 설정되어있는데 왜 여기에도 들어가야 하는가?
-    // UserEntity 기본값과 동일해야 함 
+    // 엔티티는 단지, 객체 생성 시점에만 적용되는거고 이건 삭제 시 채워줄 상수일 뿐
     private static final String PLACEHOLDER_URL = "https://via.placeholder.com/150";
 
     // 프로필사진 수정
