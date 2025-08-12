14eff79ea0cc294db08467a632c190aa49a52921
JIHUN KWAK <rhkrwlgns12@naver.com>
2025-08-12 17:48:27 +0900

    :sparkles: feat: 댓글 목록 페이징 및 정렬 기능 구현

--- src/main/java/com/example/newsfeedproject/comment/controller/CommentsController.java
+++ src/main/java/com/example/newsfeedproject/comment/controller/CommentsController.java
@@ -3,9 +3,14 @@ package com.example.newsfeedproject.comment.controller;
 import com.example.newsfeedproject.auth.impl.UserDetailsImpl;
 import com.example.newsfeedproject.comment.dto.CommentResponseDto;
 import com.example.newsfeedproject.comment.dto.CreateCommentRequestDto;
+import com.example.newsfeedproject.comment.entity.Comments;
 import com.example.newsfeedproject.comment.service.CommentsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -38,14 +43,21 @@ public class CommentsController {
     }
     // 댓글 조회 API
     @GetMapping
-    public ResponseEntity<List<CommentResponseDto>> getCommentsByFeed(
+    public ResponseEntity<Page<CommentResponseDto>> getCommentsByFeed(
             @PathVariable Long feedId,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
             @AuthenticationPrincipal UserDetailsImpl userDetailsImpl
     ) {
 
-        List<CommentResponseDto> responseDtoList = commentsService.getCommentsByFeed(feedId);
+        // 정렬 기준 추가 (최신순)
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        Pageable pageable = PageRequest.of(page, size, sort); // Pageable 객체 생성
 
-        return ResponseEntity.ok(responseDtoList);
+        // 서비스 계층을 통해 특정 게시글의 댓글 목록 조회 (Pageable 전달)
+        Page<CommentResponseDto> responseDtoPage = commentsService.getCommentsByFeed(feedId, pageable); // Pageable 전달
+
+        return ResponseEntity.ok(responseDtoPage);
     }
 
 }
