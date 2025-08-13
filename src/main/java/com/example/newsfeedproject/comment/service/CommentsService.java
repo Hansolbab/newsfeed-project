0967ab2d05c5fceede6b196095082cc095fbebf8
JIHUN KWAK <rhkrwlgns12@naver.com>
2025-08-13 13:50:26 +0900

    :fire: delete: 불필요한 import 제거

--- src/main/java/com/example/newsfeedproject/comment/service/CommentsService.java
+++ src/main/java/com/example/newsfeedproject/comment/service/CommentsService.java
@@ -15,9 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class CommentsService {
