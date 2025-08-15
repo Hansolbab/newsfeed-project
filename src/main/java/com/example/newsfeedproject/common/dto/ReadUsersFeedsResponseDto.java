2bdd331341916905d93559604f8b3c5cd2c7686b
Beforejamni <beforejamn1@gmail.com>
2025-08-15 17:00:37 +0900

    ✨ feat	: ResponseDto 변경(팔로우 및 게시물 조회)

--- src/main/java/com/example/newsfeedproject/common/dto/ReadUsersFeedsResponseDto.java
+++ src/main/java/com/example/newsfeedproject/common/dto/ReadUsersFeedsResponseDto.java
@@ -2,6 +2,7 @@ package com.example.newsfeedproject.common.dto;
 
 import com.example.newsfeedproject.category.entity.Category;
 import lombok.Getter;
+import lombok.Locked;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,14 +18,36 @@ public class ReadUsersFeedsResponseDto {
     private Integer commentTotal;
     private boolean liked;
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+//    private LocalDateTime updatedAt;
+    private String userName;
+    private String userProfileImageUrl;
+    private boolean followed;
 
-    public ReadUsersFeedsResponseDto(Long feedId, List<String> feedImgs, String contents, Integer likeTotal, Integer commentTotal, Boolean liked) {
+    public ReadUsersFeedsResponseDto(Long feedId, List<String> feedImgs, String contents, Integer likeTotal, Integer commentTotal, Boolean liked, Category category, String userName, String userProfileImageUrl, boolean followed, LocalDateTime createdAt) {
         this.feedId = feedId;
         this.feedImageURL = feedImgs;
         this.contents = contents;
         this.likeTotal = likeTotal;
         this.commentTotal = commentTotal;
         this.liked = liked;
+        this.category = category;
+        this.userName = userName;
+        this.userProfileImageUrl = userProfileImageUrl;
+        this.followed = followed;
+        this.createdAt = createdAt;
     }
+
+    public ReadUsersFeedsResponseDto(Long feedId, List<String> feeImgs, String contents, Integer likeTotal, Integer commentTotal, boolean liked){
+
+        this.feedId = feedId;
+        this.feedImageURL = feeImgs;
+        this.contents = contents;
+        this.likeTotal = likeTotal;
+        this.commentTotal = commentTotal;
+        this.liked = liked;
+
+
+    }
+
+
 }
